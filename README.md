2c3fc6a6de77dc789a1f72c01e7fdcfb5933da29
VinhLX <vinhlx@hblab.vn>
2022-07-13 14:50:39 +0700

    commit

---
+++ README.md
@@ -0,0 +1,23 @@
+<h1 align='center'>Working with canvas use Konva</h1>
+
+Project use Vite + React + Konva ⚡.
+
+## **Using 📦**
+
+1. Clone Project
+
+```
+git clone git@github.com:vinhlx-hblab/react-konva-example.git
+```
+
+2. Install Packages
+
+```
+yarn install
+```
+
+3. Start Project
+
+```
+yarn dev
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { Stage, Layer, Line, Text, Image, Shape } from "react-konva";
+import React from "react";
+import { Stage, Layer, Line, Image } from "react-konva";
 import "./App.css";
 import useImage from "use-image";
 
-const BgImage = () => {
-  const [image] = useImage(
-    "https://cdn.tgdd.vn/hoi-dap/1369909/200-anh-lam-slide-powerpoint-cuc-dep-chuyen-nghiep-khong%20(3).jpg",
-    "anonymous"
-  );
+const BgImage = ({ imageURL }) => {
+  const [image] = useImage(imageURL, "anonymous");
   return <Image image={image} />;
 };
 
@@ -23,13 +20,20 @@ function downloadURI(uri, name) {
 function App() {
   const [tool, setTool] = React.useState("pen");
   const [lines, setLines] = React.useState([]);
+  const [strokeWidth, setStrokeWidth] = React.useState(1);
+  const [strokeColor, setStrokeColor] = React.useState("#000000");
+  const [imageURL, setImageURL] = React.useState("");
+  const [showDrawContainer, setShowDrawContainer] = React.useState(false);
   const isDrawing = React.useRef(false);
   const stageRef = React.useRef(null);
 
   const handleMouseDown = (e) => {
     isDrawing.current = true;
     const pos = e.target.getStage().getPointerPosition();
-    setLines([...lines, { tool, points: [pos.x, pos.y] }]);
+    setLines([
+      ...lines,
+      { tool, points: [pos.x, pos.y], strokeWidth, strokeColor },
+    ]);
   };
 
   const handleMouseMove = (e) => {
@@ -58,62 +62,126 @@ function App() {
   };
 
   return (
-    <div>
-      <div className="position-relative">
-        <img
-          src="https://cdn.tgdd.vn/hoi-dap/1369909/200-anh-lam-slide-powerpoint-cuc-dep-chuyen-nghiep-khong%20(3).jpg"
-          onLoad={onBgImgLoad}
-        />
-        <Stage
-          onMouseDown={handleMouseDown}
-          onMousemove={handleMouseMove}
-          onMouseup={handleMouseUp}
-          style={{
-            position: "absolute",
-            top: "0",
-          }}
-          ref={stageRef}
-        >
-          <Layer>
-            <BgImage />
-          </Layer>
-          <Layer>
-            {lines.map((line, i) => (
-              <Line
-                key={i}
-                points={line.points}
-                stroke="#df4b26"
-                strokeWidth={5}
-                tension={0.5}
-                lineCap="round"
-                lineJoin="round"
-                globalCompositeOperation={
-                  line.tool === "eraser" ? "destination-out" : "source-over"
-                }
+    <div className="container py-5">
+      {!showDrawContainer && (
+        <div className="mb-3">
+          <input
+            type="text"
+            className="form-control mb-3"
+            placeholder="Enter a image url"
+            onChange={(e) => setImageURL(e.target.value)}
+          />
+          <div>
+            <button
+              onClick={() => {
+                setShowDrawContainer(true);
+              }}
+              className="btn btn-primary"
+              disabled={!imageURL}
+            >
+              Draw
+            </button>
+            <button
+              onClick={() => {
+                setImageURL(
+                  "https://cdn.tgdd.vn/hoi-dap/1369909/200-anh-lam-slide-powerpoint-cuc-dep-chuyen-nghiep-khong%20(3).jpg"
+                );
+                setShowDrawContainer(true);
+              }}
+              className="btn btn-primary ms-2"
+            >
+              Use URL example
+            </button>
+          </div>
+        </div>
+      )}
+      {showDrawContainer && (
+        <div className="row">
+          <div className="tool-container col-3">
+            <div className="mb-3">
+              <select
+                value={tool}
+                onChange={(e) => {
+                  setTool(e.target.value);
+                }}
+                className="form-control w-25"
+              >
+                <option value="pen">Pen</option>
+                <option value="eraser">Eraser</option>
+              </select>
+            </div>
+            <div className="mb-3">
+              <input
+                id="size"
+                type="range"
+                min="0.5"
+                max="100"
+                value={strokeWidth}
+                step="0.1"
+                onChange={(e) => {
+                  const { value } = e.target;
+                  setStrokeWidth(value);
+                }}
               />
-            ))}
-          </Layer>
-        </Stage>
-      </div>
-      <select
-        value={tool}
-        onChange={(e) => {
-          setTool(e.target.value);
-        }}
-        className="form-control w-25"
-      >
-        <option value="pen">Pen</option>
-        <option value="eraser">Eraser</option>
-      </select>
-      <button
-        className="btn btn-primary mt-3"
-        onClick={() => {
-          const uri = stageRef.current.toDataURL();
-          downloadURI(uri, "stage.png");
-        }}
-      >
-        Export
-      </button>
+            </div>
+            <div className="mb-3">
+              <input
+                id="color"
+                type="color"
+                value={strokeColor}
+                onChange={(e) => setStrokeColor(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  const uri = stageRef.current.toDataURL();
+                  downloadURI(uri, "stage.png");
+                }}
+              >
+                Export
+              </button>
+            </div>
+          </div>
+          <div className="position-relative col-9">
+            <img src={imageURL} onLoad={onBgImgLoad} />
+            <Stage
+              onMouseDown={handleMouseDown}
+              onMousemove={handleMouseMove}
+              onMouseup={handleMouseUp}
+              onTouchStart={handleMouseDown}
+              onTouchEnd={handleMouseUp}
+              onTouchMove={handleMouseMove}
+              style={{
+                position: "absolute",
+                top: "0",
+                cursor: "crosshair",
+              }}
+              ref={stageRef}
+            >
+              <Layer>
+                <BgImage imageURL={imageURL} />
+              </Layer>
+              <Layer>
+                {lines.map((line, i) => (
+                  <Line
+                    key={i}
+                    points={line.points}
+                    stroke={line.strokeColor}
+                    strokeWidth={line.strokeWidth}
+                    lineCap="round"
+                    lineJoin="round"
+                    globalCompositeOperation={
+                      line.tool === "eraser" ? "destination-out" : "source-over"
+                    }
+                  />
+                ))}
+              </Layer>
+            </Stage>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
